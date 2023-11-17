1226a89ec164635329e7d96bd1f163f35905879d
marcelokfws <marcelokfws@gmail.com>
2023-11-17 10:10:20 -0300

    celery

--- apps/core/tasks.py
+++ apps/core/tasks.py
@@ -19,15 +19,3 @@ def xsum(numbers):
     return sum(numbers)
 
 
-@shared_task
-def send_relatorio():
-    total = Funcionario.objects.all().count()
-    send_mail(
-        'Relatorio Celery',
-        'Relatorio geral de funcionarios %f' % total,
-        'django@gregorypacheco.com.br',
-        ['openshift@gregorypacheco.com.br'],
-        fail_silently=False,
-    )
-
-    return 'Email sent'
