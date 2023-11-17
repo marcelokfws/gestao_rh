2ef8b0228c7a8f648e8a8e0cf9d610da5d46746e
marcelokfws <marcelokfws@gmail.com>
2023-11-17 17:23:22 -0300

    task

--- gestao_rh/settings.py
+++ gestao_rh/settings.py
@@ -129,13 +129,19 @@ CELERY_TASK_SERIALIZER = 'json'
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
-EMAIL_HOST = config('EMAIL_HOST')
-EMAIL_PORT = config('EMAIL_PORT', cast=int)
-EMAIL_HOST_USER = config('EMAIL_HOST_USER')
-EMAIL_HOST_PASSWORD = config('EMAIL_HOST_PASSWORD')
+#EMAIL_HOST = config('EMAIL_HOST')
+#EMAIL_PORT = config('EMAIL_PORT', cast=int)
+#EMAIL_HOST_USER = config('EMAIL_HOST_USER')
+#EMAIL_HOST_PASSWORD = config('EMAIL_HOST_PASSWORD')
 #EMAIL_USE_SSL = True
 EMAIL_USE_TLS = True
 
 
 
-
+EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
+EMAIL_HOST = 'smtp.gmail.com'
+EMAIL_HOST_USER = 'marceloesileia@gmail.com'
+EMAIL_HOST_PASSWORD = 'pkqzalkakjlzdgvh'
+EMAIL_PORT = 587
+EMAIL_USE_TLS = True
+DEFAULT_FROM_EMAIL = 'Lista de Funcionários <django.marceloesileia@gmail.com>'
