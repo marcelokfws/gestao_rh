589619af16bf4b5b30b326e8bb8608a1b552ed09
marcelokfws <marcelokfws@gmail.com>
2023-11-17 10:22:39 -0300

    t

--- gestao_rh/settings.py
+++ gestao_rh/settings.py
@@ -122,7 +122,7 @@ LOGIN_REDIRECT_URL = 'home'
 LOGOUT_REDIRECT_URL = 'login'
 
 CELERY_RESULT_BACKEND = 'django-db'
-
+CELERY_BROKER_URL = 'redis://localhost:6379'
 CELERY_ACCEPT_CONTENT = ['application/json']
 CELERY_RESULT_SERIALIZER = 'json'
 CELERY_TASK_SERIALIZER = 'json'
