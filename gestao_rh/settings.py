78fedea311ed2f6cf848e7c2ca42000ef5869895
marcelokfws <marcelokfws@gmail.com>
2023-11-17 09:48:06 -0300

    celery

--- gestao_rh/settings.py
+++ gestao_rh/settings.py
@@ -19,6 +19,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'django_celery_results',
     'apps.empresas',
     'apps.funcionarios',
     'apps.departamentos',
@@ -119,5 +120,11 @@ MEDIA_ROOT = os.path.join(BASE_DIR, "media")
 LOGIN_REDIRECT_URL = 'home'
 
 LOGOUT_REDIRECT_URL = 'login'
+
+CELERY_RESULT_BACKEND = 'django-db'
+
+CELERY_ACCEPT_CONTENT = ['application/json']
+CELERY_RESULT_SERIALIZER = 'json'
+CELERY_TASK_SERIALIZER = 'json'
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
