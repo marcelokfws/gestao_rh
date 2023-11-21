35d8f065557243b7762f930ba47d222f74deab06
marcelokfws <marcelokfws@gmail.com>
2023-11-21 10:05:46 -0300

    local-settings

--- gestao_rh/settings.py
+++ gestao_rh/settings.py
@@ -128,19 +128,7 @@ DATABASE_ROUTERS = ['gestao_rh.DBRoutes.DBRoutes']
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
-#EMAIL_HOST = config('EMAIL_HOST')
-#EMAIL_PORT = config('EMAIL_PORT', cast=int)
-#EMAIL_HOST_USER = config('EMAIL_HOST_USER')
-#EMAIL_HOST_PASSWORD = config('EMAIL_HOST_PASSWORD')
-#EMAIL_USE_SSL = True
-EMAIL_USE_TLS = True
 
 
 
-EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
-EMAIL_HOST = 'smtp.gmail.com'
-EMAIL_HOST_USER = 'marceloesileia@gmail.com'
-EMAIL_HOST_PASSWORD = 'pkqzalkakjlzdgvh'
-EMAIL_PORT = 587
-EMAIL_USE_TLS = True
-DEFAULT_FROM_EMAIL = 'Lista de Funcionários <django.marceloesileia@gmail.com>'
+
